decd901426f2b882180b6a83fbc5ffe087995154
Ravi Vyas <ravivyas84@gmail.com>
2011-04-20 23:56:42 +0530

    Addded Android files & gitignore

---
+++ trunk/src/com/ravivyas/stackapps/ui/LandingActivity.java
@@ -0,0 +1,16 @@
+package com.ravivyas.stackapps.ui;
+
+import com.ravivyas.stackapps.R;
+import com.ravivyas.stackapps.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LandingActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
